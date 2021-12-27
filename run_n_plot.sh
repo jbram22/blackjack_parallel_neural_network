4fd1d791de4fe85bb7bfe03a0985d8919f49c7ce
jbram22 <84873339+jbram22@users.noreply.github.com>
2021-12-27 13:02:30 -0500

    shell script to train both nn(s) and plot scalability analysis results

--- run_n_plot.sh
+++ run_n_plot.sh
@@ -6,4 +6,4 @@ mpirun -n 4 python3 -m mpi4py data_parallel_nn.py
 mpirun -n 3 python3 -m mpi4py data_parallel_nn.py
 mpirun -n 2 python3 -m mpi4py data_parallel_nn.py
 
-python3 plotting.py
+python3 plotting.py 
